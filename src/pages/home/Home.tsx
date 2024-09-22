28c6fc6d36502308307f4476ee32eecb300ef0b7
ipscoder <psawant0015@gmail.com>
2024-09-22 14:27:13 +0530

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+import { Box, Typography } from '@mui/material'
+import UITable from "../../ui/table/UITable"
+import React from 'react'
+
+const Home = () => {
+    
+
+
+  return (
+    <Box>
+        <Typography variant='h6' >
+            This Is Basics
+        </Typography>
+        <UITable/>
+    </Box>
+  )
+}
+
+export default Home
